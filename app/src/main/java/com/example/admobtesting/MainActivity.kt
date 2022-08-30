60f8ea73cb0117a416c237bdf777664333129d78
Abdullah <abdullahazhar971@gmail.com>
2022-08-30 17:18:43 +0500

    first commit

---
+++ app/src/main/java/com/example/admobtesting/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.admobtesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.google.android.gms.ads.rewarded.RewardItem
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
+
+class MainActivity : AppCompatActivity() {
+
+    var mInterstitialAd: InterstitialAd? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lateinit var mAdView : AdView
+
+
+
+        /* ca-app-pub-3560802488472325/6075651003 */
+
+
+
+        findViewById<TextView>(R.id.tvbtn).text=BuildConfig.AD_ID
+
+        val adRequest= AdRequest.Builder().build()
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d("messi3", "Interstial error")
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d("messi3", "Interstial Loaded")
+                mInterstitialAd = interstitialAd
+
+            }
+        })
+
+        mAdView = findViewById(R.id.adView)
+        mAdView.loadAd(adRequest)
+
+        mAdView.adListener = object: AdListener() {
+            override fun onAdClicked() {
+            }
+
+            override fun onAdClosed() {
+
+            }
+
+            override fun onAdFailedToLoad(adError : LoadAdError) {
+            }
+
+            override fun onAdImpression() {
+
+            }
+
+            override fun onAdLoaded() {
+            }
+
+            override fun onAdOpened() {
+            }
+        }
+
+
+
+
+
+        findViewById<TextView>(R.id.tvbtn).setOnClickListener {
+            mInterstitialAd?.show(this@MainActivity)
+        }
+
+
+
+
+    }
+}
